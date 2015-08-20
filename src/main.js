7565496115e34efb6d7c4cf517f99f7a278c7af8
shtch <oleg.shcherbatykh@mail.ru>
2015-08-20 17:52:34 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
 var page = tabris.create("Page", {
-  title: "Web View",
+  title: "Асклепиус справка",
   topLevel: true
 });
 
-var urlInput = tabris.create("TextInput", {
-  layoutData: {left: 8, right: 8, top: 8},
-  message: "Enter URL...",
-  text: "http://http://rap.eclipsesource.com/demo/release/rapdemo/"
-}).on("accept", loadUrl).appendTo(page);
-
 var webview = tabris.create("WebView", {
-  layoutData: {left: 0, top: [urlInput, 8], right: 0, bottom: 0}
+  layoutData: {left: 0, top: 0, right: 0, bottom: 0}
 }).appendTo(page);
 
 function loadUrl() {
-  webview.set("url", urlInput.get("text"));
+  webview.set("url", "http://94.79.10.35:9740/rinfo/std");
 }
 
 loadUrl();
